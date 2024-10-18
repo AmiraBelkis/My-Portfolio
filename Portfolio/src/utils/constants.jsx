07bafc549fbd8f341558bb7913f4ea182319c67a
Amira Belkis <ia_feliachi@esi.dz>
2024-10-18 19:01:47 +0100

    add another icon

--- Portfolio/src/utils/constants.jsx
+++ Portfolio/src/utils/constants.jsx
@@ -1,4 +1,4 @@
-import { UpWorkAccount, GithubAccount, LinkedInAccount } from '../ui/Icon'
+import { UpWorkAccount, GithubAccount, LinkedInAccount, MailAccount } from '../ui/Icon'
 
 export const menuItems = [
     { id: 1, title: 'Home', link: '/#home' },
@@ -8,11 +8,15 @@ export const menuItems = [
     { id: 5, title: 'Contact', link: '/#footer' }
 ];
 
-export const profiles = [{ icon: "github", link: "https://github.com/AmiraBelkis" },
-{ icon: "linkedin", link: "https://www.linkedin.com/in/amirabelkis/" },
-{ icon: "upwork", link: "https://www.upwork.com/freelancers/~012dd94f449054402d" }]
+export const profiles = [
+    { icon: "mail", link: "mailto:fe.amirabelkis@gmail.com" },
+    { icon: "github", link: "https://github.com/AmiraBelkis" },
+    { icon: "linkedin", link: "https://www.linkedin.com/in/amirabelkis/" },
+    { icon: "upwork", link: "https://www.upwork.com/freelancers/~012dd94f449054402d" }
+]
 
 export const profileIcons = {
+    "mail": <MailAccount />,
     "github": <GithubAccount />,
     "linkedin": <LinkedInAccount />,
     "upwork": <UpWorkAccount />,
