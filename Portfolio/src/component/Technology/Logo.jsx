70e9e57f5acbd8c3d85bc8b0a812789973264451
Amira Belkis <ia_feliachi@esi.dz>
2024-10-23 14:24:58 +0100

    update card style for displaying images and cards in same ligne

--- Portfolio/src/component/Technology/Logo.jsx
+++ Portfolio/src/component/Technology/Logo.jsx
@@ -1,7 +1,7 @@
 export const Logo = ({ img }) => {
     return (
-        <div className="p-2 text-center d-flex">
-            <img src={img} alt="Logo" width="128px" />
+        <div className="p-2 text-center d-flex justify-content-center">
+            <img src={img} alt="Logo" />
         </div>
     )
 
