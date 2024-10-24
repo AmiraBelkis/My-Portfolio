48dc18d697b16572112e1f17b554f922bfa0b86f
Amira Belkis <ia_feliachi@esi.dz>
2024-10-24 17:28:38 +0100

    sending email with emailjs

--- Portfolio/src/component/Footer/ContactForm.jsx
+++ Portfolio/src/component/Footer/ContactForm.jsx
@@ -1,13 +1,32 @@
-import { Title } from "../../ui/Title";
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 export const ContactForm = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_4yw9oxy', 'template_h1krtp4', form.current, {
+                publicKey: 'T6wWspf3dg5OUsz78',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
     return (
         <>
-            <form className="row gap-2 justify-content-end">
+            <form ref={form} onSubmit={sendEmail} className="row gap-2 justify-content-end">
                 <h3 className="form-title col-12">  Contact Me </h3>
                 <input type="text" required name="name" placeholder="Name" className="col-12 form-input"></input>
                 <input type="email" required name="email" placeholder="Email" className="col-12 form-input"></input>
-                <textarea placeholder="Message..." required rows="4" className="col-12 form-input"></textarea>
+                <textarea name="message" placeholder="Message..." required rows="4" className="col-12 form-input"></textarea>
                 <button type="submit" className="col-auto btn-submit">Send</button>
             </form>
         </>
