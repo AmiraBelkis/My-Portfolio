e69320b9b05eaa6f5bf70308dfee023769aec4d7
Amira Belkis <ia_feliachi@esi.dz>
2024-10-24 14:45:44 +0100

    fix some issues in style footer

--- Portfolio/src/component/Footer/ContactForm.jsx
+++ Portfolio/src/component/Footer/ContactForm.jsx
@@ -6,7 +6,7 @@ export const ContactForm = () => {
             <form className="row gap-2 justify-content-end">
                 <h3 className="form-title col-12">  Contact Me </h3>
                 <input type="text" required name="name" placeholder="Name" className="col-12 form-input"></input>
-                <input type="mail" required name="email" placeholder="Email" className="col-12 form-input"></input>
+                <input type="email" required name="email" placeholder="Email" className="col-12 form-input"></input>
                 <textarea placeholder="Message..." required rows="4" className="col-12 form-input"></textarea>
                 <button type="submit" className="col-auto btn-submit">Send</button>
             </form>
--- Portfolio/src/component/Footer/Menu.jsx
+++ Portfolio/src/component/Footer/Menu.jsx
@@ -3,7 +3,7 @@ import { menuItems } from "../../utils/constants"
 export const Menu = () => {
     return <>
         <hr className="separator" />
-        <div className="d-flex gap-4 p-0">
+        <div className="d-flex p-0" style={{ justifyContent: "space-between" }}>
             {
                 menuItems.map((item, index) =>
                     <a className="footer-menu-item" key={index} href={item.link}>{item.title} </a>
