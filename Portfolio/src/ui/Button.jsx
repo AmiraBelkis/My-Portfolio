e19c0f521456e35dad451a7f35fe25d352586d13
Amira Belkis <ia_feliachi@esi.dz>
2024-10-24 15:57:21 +0100

    fix btn issue for mobile view

--- Portfolio/src/ui/Button.jsx
+++ Portfolio/src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import { RightArrow } from "./Icon";
 export const DownloadButton = ({ filePath, fileName, title }) => {
     return (
-        <a className="col-2 btn btn-primary m-2" role="button" href={filePath} download={fileName}  >{title}</a>
+        <a className="col-auto btn btn-primary m-2" role="button" href={filePath} download={fileName}  >{title}</a>
 
 
     );
@@ -9,7 +9,7 @@ export const DownloadButton = ({ filePath, fileName, title }) => {
 
 export const LinkButton = ({ title, link }) => {
     return (
-        <a className="col-2  btn-secondary m-2" role="button" href={link}>
+        <a className="col-auto  btn-secondary m-2" role="button" href={link}>
             <span className="m-2">{title}</span>
             <RightArrow></RightArrow>
         </a>
