30789e1d408d3bd953e12312d54bdee2e1e729f8
Amira Belkis <ia_feliachi@esi.dz>
2024-10-24 15:19:16 +0100

    fix equal stack issue

--- Portfolio/src/component/Projects/ProjectItem.jsx
+++ Portfolio/src/component/Projects/ProjectItem.jsx
@@ -2,17 +2,20 @@ import { Card } from "../../ui/Card";
 import { Description } from "./Description";
 import { TechStack } from "./TechStack";
 import { Link } from "./Link";
+import { useWindowWidth } from "../../hooks/useWindowWidth";
 
 export const ProjectItem = ({ name, image, description, tech, githubLink, LiveReviewLink }) => {
+    const sizePurcentage = 1440 / useWindowWidth();
+
     return (
         <Card col="col-sm-auto">
             <img src={`/src/assets/img/prjt/${image}`} alt="Project-img" className="prjt-img" />
             <div className="container p-3">
-                <h4 className="d-flex align-items-center justify-content-center" style={{ height: "60px" }}>{name}</h4>
-                <Description props={{ style: { height: "65px" } }} >
+                <h4 className="d-flex align-items-center justify-content-center" style={{ height: `${50 * sizePurcentage}px` }}>{name}</h4>
+                <Description props={{ style: { height: `${65 * sizePurcentage}px` } }} >
                     {description}
                 </Description>
-                <TechStack props={{ style: { height: "50px" } }}>
+                <TechStack props={{ style: { height: `${50 * sizePurcentage}px` } }}>
                     {tech}
                 </TechStack>
                 <div className="d-flex">
