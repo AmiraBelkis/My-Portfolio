7f86317822e08317c83c5a79f46b850e6becc2bb
Amira Belkis <ia_feliachi@esi.dz>
2024-10-25 12:09:53 +0100

    add loading state to the contact form

--- Portfolio/src/component/Footer/ContactForm.jsx
+++ Portfolio/src/component/Footer/ContactForm.jsx
@@ -5,13 +5,14 @@ import { alertTypes } from '../../utils/constants';
 
 export const ContactForm = () => {
     const form = useRef();
+    const [loading, setLoading] = useState(false);
     const [alert, setAlert] = useState({
         show: false,
         ...alertTypes.success
     });
     const sendEmail = (e) => {
         e.preventDefault();
-
+        setLoading(true);
         emailjs
             .sendForm('service_4yw9oxy', 'template_h1krtp4', form.current, {
                 publicKey: 'T6wWspf3dg5OUsz78',
@@ -22,12 +23,14 @@ export const ContactForm = () => {
                         show: true,
                         ...alertTypes.success
                     })
+                    setLoading(false);
                 },
                 (error) => {
                     setAlert({
                         show: true,
                         ...alertTypes.danger
                     })
+                    setLoading(false);
                 },
             );
     };
@@ -38,7 +41,10 @@ export const ContactForm = () => {
                 <input type="text" required name="name" placeholder="Name" className="col-12 form-input"></input>
                 <input type="email" required name="email" placeholder="Email" className="col-12 form-input"></input>
                 <textarea name="message" placeholder="Message..." required rows="4" className="col-12 form-input"></textarea>
-                <button type="submit" className="col-auto btn-submit">Send</button>
+                <button type="submit"
+                    className={`col-auto btn-submit ${loading ? "active" : ""}`}>
+                    {loading ? "Sending..." : "Send"}
+                </button>
             </form>
             <Alert className='form-alert' {...alert} onClose={() => setAlert({
                 show: false,
